bdef1c3a03e2e1e0981afd0a98e16e49c977aa7d
AliAkbar <aliakbarranjdoost@gmail.com>
2023-11-05 08:56:34 +0330

    Pathway 1 - Activity 6 - Part 5 : Align your body - Alignment

--- app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
+++ app/src/main/java/com/codelab/basiclayouts/MainActivity.kt
@@ -21,9 +21,14 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.annotation.DrawableRes
 import androidx.annotation.StringRes
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material3.Icon
@@ -32,7 +37,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextField
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -50,22 +59,6 @@ class MainActivity : ComponentActivity() {
 fun SearchBar(
     modifier: Modifier = Modifier
 ) {
-    // Implement composable here
-}
-
-// Step: Align your body - Alignment
-@Composable
-fun AlignYourBodyElement(
-    modifier: Modifier = Modifier
-) {
-    // Implement composable here
-}
-
-// Step: Favorite collection card - Material Surface
-@Composable
-fun FavoriteCollectionCard(
-    modifier: Modifier = Modifier
-) {
     TextField(
         value = "",
         placeholder = { Text(text = stringResource(id = R.string.placeholder_search)) },
@@ -78,11 +71,46 @@ fun FavoriteCollectionCard(
             Icon(
                 imageVector = Icons.Default.Search,
                 contentDescription = Icons.Default.Search.name)
-                      },
+        },
         modifier = modifier
             .fillMaxWidth()
             .heightIn(min = 54.dp)
     )
+}
+
+// Step: Align your body - Alignment
+@Composable
+fun AlignYourBodyElement(@DrawableRes drawable: Int,
+                         @StringRes text: Int,
+                         modifier: Modifier = Modifier
+)
+{
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(id = drawable),
+            contentDescription = null,
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .size(88.dp)
+                .clip(CircleShape)
+        )
+        Text(
+            text = stringResource(id = text),
+            modifier = Modifier.paddingFromBaseline(top = 24.dp, bottom = 8.dp),
+            style = MaterialTheme.typography.bodyMedium
+        )
+    }
+}
+
+// Step: Favorite collection card - Material Surface
+@Composable
+fun FavoriteCollectionCard(
+    modifier: Modifier = Modifier
+) {
+
 }
 
 // Step: Align your body row - Arrangements
@@ -179,6 +207,8 @@ fun SearchBarPreview() {
 fun AlignYourBodyElementPreview() {
     MySootheTheme {
         AlignYourBodyElement(
+            text = R.string.ab1_inversions,
+            drawable = R.drawable.ab1_inversions,
             modifier = Modifier.padding(8.dp)
         )
     }
